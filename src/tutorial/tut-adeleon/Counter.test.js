872293119deaf699960caca1d27ad9da5899df58
AC De Leon <aadeleon@sdsu.edu>
2018-09-16 21:20:18 -0700

    [*] Tutorial: Pull Request

--- src/tutorial/tut-adeleon/Counter.test.js
+++ src/tutorial/tut-adeleon/Counter.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Counter from "./Counter";
+import * as enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+enzyme.configure({ adapter: new Adapter() });
 
 it("increments count correctly after the button is pressed", () => {
   const wrapper = shallow(<Counter />);
