f9ca25276b31bacd5ceb9e9d59450ec04bc6ffb3
Kevin Do <kevindokhoale@gmail.com>
2018-09-20 20:01:47 -0700

    Minor verifications of firestore connect

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import firebase from "./firebaseConfig";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import Greeter from "./Greeter.js";
 
-// Configure FirebaseUI.
+// Configure FirebaseUI
 let uiConfig = {
-  // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/signedIn",
-  // We will display Google and Facebook as auth providers.
-
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
