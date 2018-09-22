cc2db0ebf1cec8ba90375ec4a914e3cca29fd0e2
Kevin Do <kevindokhoale@gmail.com>
2018-09-21 17:28:09 -0700

    Simplified User Document

--- functions/index.js
+++ functions/index.js
@@ -6,31 +6,23 @@ const firestore = admin.firestore();
 firestore.settings({ timestampsInSnapshots: true });
 
 exports.createUserDocument = functions.auth.user().onCreate(user => {
-    const { customClaims, disabled, displayName, email, emailVerified, photoURL, uid } = user;
-    const { creationTime } = user.metadata;
-    const { displayName: providerName, email: providerEmail, photoURL: providerPhotoURL, providerId, uid: providerUid } = user.providerData[0];
-    return firestore
-        .collection("users")
-        .doc(user.uid)
-        .set({
-            customClaims,
-            disabled,
-            displayName,
-            email,
-            emailVerified,
-            photoURL,
-            uid,
-            creationTime,
-            providerName,
-            providerEmail,
-            providerPhotoURL,
-            providerId,
-            providerUid
-        })
-        .then(() => {
-            return console.log("Write succeeded!");
-        })
-        .catch(error => {
-            console.log(error.message);
-        });
+  const { displayName, providerData, email } = user;
+  const { photoURL } = providerData[0];
+
+  return firestore
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      exp: 0,
+      role: "user",
+      displayName,
+      photoURL,
+      email
+    })
+    .then(() => {
+      return console.log("Write succeeded!");
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
 });
