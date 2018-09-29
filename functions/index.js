697435e3d3e3e62aad1c269b09d78f01f6bc9fa1
Kevin Do <kevindokhoale@gmail.com>
2018-09-28 18:13:13 -0700

    Pusher Account Creation
    + Hooked onto firebase auth

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,28 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const Chatkit = require("@pusher/chatkit-server");
 
+// Initialize firestore
 admin.initializeApp();
 const firestore = admin.firestore();
+
+// Environment Variables
+const { instancelocator, key } = functions.config().chatkit;
+
+const chatkit = new Chatkit.default({
+  instanceLocator: instancelocator,
+  key: key
+});
+
 firestore.settings({ timestampsInSnapshots: true });
 
 exports.createUserDocument = functions.auth.user().onCreate(user => {
-  const { displayName, providerData, email } = user;
+  const { displayName, email, providerData, uid } = user;
   const { photoURL } = providerData[0];
 
   return firestore
     .collection("users")
-    .doc(user.uid)
+    .doc(uid)
     .set({
       exp: 0,
       role: "user",
@@ -24,5 +35,23 @@ exports.createUserDocument = functions.auth.user().onCreate(user => {
     })
     .catch(error => {
       console.log(error.message);
+    });
+});
+
+exports.createChatkitAccount = functions.auth.user().onCreate(user => {
+  const { displayName, providerData, uid } = user;
+  const { photoURL } = providerData[0];
+
+  return chatkit
+    .createUser({
+      id: uid,
+      name: displayName,
+      avatar_url: photoURL
+    })
+    .then(() => {
+      return console.log("User created successfully");
+    })
+    .catch(err => {
+      console.log(err);
     });
 });
