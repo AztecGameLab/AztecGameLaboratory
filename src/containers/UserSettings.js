a3df7139b52938ef6d5f737bb94964892539dc74
AC De Leon <aadeleon@sdsu.edu>
2018-10-04 18:56:06 -0700

    [+] userSettings.js, form functionality

---
+++ src/containers/UserSettings.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class UserSettings extends Component {
+  state = {
+    avatar: null,
+    username: null,
+    oldPassword: null,
+    newPassword: null,
+    destroyAccount: null
+  };
+
+  handleChange = e => {
+    // console.log(e.target.value);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Changes saved!");
+  };
+
+  handleDestroy = e => {
+    console.log("ACCOUNT DESTROYED!");
+  };
+
+  handleUpload = e => {
+    console.log("Changing avatar...");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>USER SETTINGS</h1>
+        {/* Are we handling the avatar itself, can we call on firebase api? */}
+        <label htmlFor="avatar">Change avatar:</label>
+        <button id="avatar" onClick={this.handleUpload}>
+          Upload new avatar!
+        </button>
+        <br />
+        {/* onSubmit vs onClick? */}
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" onChange={this.handleChange} />
+          <br />
+          <label htmlFor="oldPassword">Old Password::</label>
+          <input type="text" id="oldPassword" onChange={this.handleChange} />
+          <br />
+          <label htmlFor="newPassword">New Password:</label>
+          <input type="text" id="newPassword" onChange={this.handleChange} />
+          <br />
+          <button>Save changes!</button>
+        </form>
+        <label htmlFor="destroyAccount">Destroy Account?</label>
+        <button id="destroyAccount" onClick={this.handleDestroy}>
+          DESTROY!
+        </button>
+      </div>
+    );
+  }
+}
+
+export default UserSettings;
