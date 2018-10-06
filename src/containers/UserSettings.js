997253bc3c8fc9f1556bb0dc0e12e134ed15ea5e
AC De Leon <aadeleon@sdsu.edu>
2018-10-05 21:49:15 -0700

    [*] Works as intended for avatar/username

--- src/containers/UserSettings.js
+++ src/containers/UserSettings.js
@@ -3,38 +3,43 @@ import { connect } from "react-redux";
 import { withFirebase } from "react-redux-firebase";
 
 import { isLoggedIn } from "../redux/selectors";
-
 class UserSettings extends Component {
   state = {
     avatar: null,
-    username: null,
-    deleteAccount: null
+    username: null
+    // deleteAccount: null
   };
 
   handleAvatar = e => {
-    this.setState({
-      avatar: URL.createObjectURL(e.target.files[0])
-    });
+    e.preventDefault();
+    this.props.firebase.updateProfile({ photoURL: this.state.avatar });
+    e.target.reset();
+  };
+
+  handlePreAvatar = e => {
+    // this.setState({
+    //   avatar: URL.createObjectURL(e.target.files[0])
+    // });
   };
 
   handleUsername = e => {
     e.preventDefault();
     this.props.firebase.updateProfile({ displayName: this.state.username });
-    console.log("Username is now: " + this.state.username);
+    e.target.reset();
   };
 
   handleDeleteAccount = e => {
-    // this.props.firebase.delete();
-    console.log("Account deleted.");
-  };
+    console.log(this.props.firebase);
 
-  test = e => {
-    console.log(this.props);
+    // Need to be able to delete account from auth & database
+    // this.props.firebase.delete() does not work alongside re-auth-ing user
+
+    console.log("End of function.");
   };
 
   handleChange = e => {
     this.setState({
-      // Get the direct value based on id (check inputs)
+      // id is within the input method
       [e.target.id]: e.target.value
     });
   };
@@ -49,11 +54,29 @@ class UserSettings extends Component {
             {/* PROFILE SETTINGS */}
             <h1>PROFILE</h1>
 
+            {/* CURRENT AVATAR */}
+            <img src={this.props.profile.photoURL} alt="" height="300" width="300" />
+
+            {/* CURRENT SETTINGS */}
+            <p>
+              Name: {this.props.profile.displayName}
+              <br />
+              Email: {this.props.profile.email}
+              <br />
+              EXP: {this.props.profile.exp}
+              <br />
+              PhotoURL: {this.props.profile.photoURL}
+              <br />
+              Role: {this.props.profile.role}
+              <br />
+            </p>
+
             {/* AVATAR */}
-            <h2>Avatar</h2>
-            <form onSubmit={this.handleUpdateProfile}>
-              <input type="file" onChange={this.handleAvatar} />
-              <img src={this.state.avatar} alt="" />
+            <h2>Change avatar (URL)</h2>
+            <form onSubmit={this.handleAvatar}>
+              <input type="text" id="avatar" onChange={this.handleChange} />
+              {/* <img src={this.state.avatar} alt="" /> */}
+              <button>Change picture</button>
             </form>
 
             {/* ACCOUNT SETTINGS */}
@@ -71,16 +94,15 @@ class UserSettings extends Component {
             </form>
 
             {/* DELETE ACCOUNT */}
-            <h2>Delete account</h2>
+            {/* <h2>Delete account</h2>
             <p>Once you delete your account, there is no going back. Please be certain. </p>
             <button
-              id="deleteAccount"
               onClick={e => {
                 if (window.confirm("Are you sure?")) this.handleDeleteAccount(e);
               }}
             >
               Delete your account
-            </button>
+            </button> */}
           </div>
         ) : null}
       </div>
