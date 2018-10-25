51b9c5419334d47a36a5b5ceb46de241a8a3096e
AC De Leon <aadeleon@sdsu.edu>
2018-10-25 16:48:20 -0700

    [+] console.log retrieves information

--- src/containers/CreatePost.js
+++ src/containers/CreatePost.js
@@ -4,10 +4,49 @@ import { withFirebase } from "react-redux-firebase";
 
 import { isLoggedIn } from "../redux/selectors";
 class UserSettings extends Component {
+  state = {
+    title: "",
+    description: "",
+    url: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    // Returns to the home page after creating a project
+    this.props.history.push("/");
+  };
+
   render() {
     const { isLoggedIn } = this.props;
 
-    return <div>{isLoggedIn ? <div>CREATE POST</div> : null}</div>;
+    return (
+      <div>
+        {isLoggedIn ? (
+          <div>
+            {/* CHANGE TO PROVIDE MORE OPTIONS RATHER THAN ART */}
+            <h1>CREATE ART</h1>
+            <form onSubmit={this.handleSubmit}>
+              <h2>TITLE</h2>
+              <input type="text" id="title" onChange={this.handleChange} />
+              <h2>DESCRIPTION</h2>
+              <textarea id="description" onChange={this.handleChange} />
+              <h2>URL</h2>
+              <input type="text" id="url" onChange={this.handleChange} />
+              <div>
+                <button>Share your creation!</button>
+              </div>
+            </form>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
